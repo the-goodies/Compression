1fe11c16f3b39f8cc62f1584792a65bcbfe10b43
Gedis <gediminas.ziauberis@gmail.com>
2017-01-03 00:31:02 +0100

    Add decompression function and some fixes

--- compression.h
+++ compression.h
@@ -23,8 +23,17 @@ struct HuffmanNode
 // comparison function so that priorityQueue can compare huffman nodes
 struct compare_huffmanNodes { bool operator()(HuffmanNode* lhs, HuffmanNode* rhs) { return lhs->freq < rhs->freq; } };
 
+
+static void clearTree(HuffmanNode* tree)
+{
+	if (tree == nullptr) return;
+	clearTree(tree->zero);
+	clearTree(tree->one);
+	delete tree;
+}
+
 // freqTable size should be 256 -> one position to store freq for each byte
-void buildFrequencyTable(ifbitstream & stream, s32* freqTable)
+static void buildFrequencyTable(ifbitstream & stream, s32* freqTable)
 {
 	for (s32 s = 0; s < 256; ++s) freqTable[s] = 0;
 
@@ -36,7 +45,7 @@ void buildFrequencyTable(ifbitstream & stream, s32* freqTable)
 }
 
 
-HuffmanNode* buildHuffmanTree(s32* freqTable)
+static HuffmanNode* buildHuffmanTree(s32* freqTable)
 {
 	// fill HuffmanForest with symbols from freqTable
 	PriorityQueue<HuffmanNode*, compare_huffmanNodes> HuffmanForest;
@@ -63,28 +72,28 @@ HuffmanNode* buildHuffmanTree(s32* freqTable)
 	return HuffmanForest.get();
 }
 
-// writes huffmanTree to a compressed file as a header
+// writes tree to a compressed file as a header
 // so that receiver could use it to expand said file
-void writeHuffmanTree(ofbitstream & stream, HuffmanNode* HuffmanTree)
+static void writeHuffmanTree(ofbitstream & stream, HuffmanNode* tree)
 {
 	// base case, when geting to a leaf, write one and a byte
-	if (HuffmanTree->isLeaf())
+	if (tree->isLeaf())
 	{
 		stream.writeBit(true); // write one
-		stream.writeByte(HuffmanTree->symbol); // then symbol
+		stream.writeByte(tree->symbol); // then symbol
 		return;
 	}
 
 	// otherwise it's internal node, write zero and
 	// recursively write other subtree's info to a stream
 	stream.writeBit(false);
-	writeHuffmanTree(stream, HuffmanTree->zero);
-	writeHuffmanTree(stream, HuffmanTree->one);
+	writeHuffmanTree(stream, tree->zero);
+	writeHuffmanTree(stream, tree->one);
 }
 
 // reads and returns encoded huffman tree from a header of given 
 // compressed file to use it to expand said file
-HuffmanNode* readHuffmanTree(ifbitstream & stream)
+static HuffmanNode* readHuffmanTree(ifbitstream & stream)
 {
 	// base case, bit one indicates a leaf, so next byte is a symbol
 	if (stream.readBit() == 1) return new HuffmanNode(stream.readByte());
@@ -98,17 +107,17 @@ HuffmanNode* readHuffmanTree(ifbitstream & stream)
 
 
 // makes a symbol table maping symbols to codewords: used for encoding a file
-void buildEncodingMap(HuffmanNode* HuffmanTree, std::string st[], std::string codeword = "")
+static void buildEncodingMap(HuffmanNode* tree, std::string st[], std::string codeword = "")
 {
 	// base case
-	if (HuffmanTree->isLeaf())
+	if (tree->isLeaf())
 	{
-		st[HuffmanTree->symbol] = codeword;
+		st[tree->symbol] = codeword;
 		return;
 	}
 	// recursive step traversing tree
-	buildEncodingMap(HuffmanTree->zero, st, codeword + "0");
-	buildEncodingMap(HuffmanTree->one, st, codeword + "1");
+	buildEncodingMap(tree->zero, st, codeword + "0");
+	buildEncodingMap(tree->one, st, codeword + "1");
 }
 
 // compresses file from instream to outstream
@@ -124,6 +133,9 @@ void compress(ifbitstream & instream, ofbitstream & outstream)
 
 	// write a tree in order for a decoder to be able to expand
 	writeHuffmanTree(outstream, root);
+
+	// release huffman tree, since we dont need it anymore
+	clearTree(root);
 
 	// write ammount of symbols overall in a file
 	// so decoder will know when to stop reading
@@ -143,6 +155,32 @@ void compress(ifbitstream & instream, ofbitstream & outstream)
 	instream.close();
 	outstream.close();
 }
+
+// decompress file from instream to outstream
+void decompress(ifbitstream & instream, ofbitstream & outstream)
+{
+	// extract huffman tree from an encoded stream
+	HuffmanNode* root = readHuffmanTree(instream);
+	// get how many symbols are in an encoded stream
+	s64 size = instream.readFourBytes();
+	// decode encoded stream
+	for (s64 pos = 0; pos < size; ++pos)
+	{
+		HuffmanNode* tree = root;
+		while(!tree->isLeaf())
+		{
+			if (instream.readBit())
+				tree = tree->one;
+			else
+				tree = tree->zero;
+		}
+		outstream.writeByte(tree->symbol);
+	}
+	instream.close();
+	outstream.close();
+}
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,48 +1,14 @@
-#include "lib/Array.h"
-#include "lib/PriorityQueue.h"
-//#include "bitstream.h"
+#include "lib/bitstream.h"
 #include "compression.h"
 
 using std::cout;
 using std::endl;
 using std::cin;
 
-template <typename cmp>
-bool compare(const cmp & a,const cmp & b)
-{
-	cout << *a << " " << *b;
-	return true;
-}
-
-template <typename compareType>
-void foo(bool (*cmp)(const compareType & a,const compareType & b), compareType a, compareType b)
-{
-	cmp(a,b);
-}
-
-
-#include "lib/sorting.h"
 
 int main()
 {
-	int* a = new int(10);
-	int* b = new int(20);
-	int c = 5;
-	int d = 10;
-	foo(compare, a, b);
-	Array<int> la{ 2, 3, 1, -12, 41, 421, 42 };
-	mergeSort(la);
-	cout << "array: " << la << endl;
 	
-	ifbitstream instream("taip.txt");
-	ofbitstream outstream("out.txt");
-	compress(instream, outstream);
-
-	instream.open("out.txt");
-	for (int i = 0; i < instream.size() * 8; ++i)
-	{
-		cout << i << ". " << instream.readBit() << endl;
-	}
 	
 	system("pause");
 }
